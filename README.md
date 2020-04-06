9d68ef9f1d1d451c0bd3d991fa7aac7f1bb2014d
oceane97440 <sautronoceane98@gmail.com>
2020-04-06 14:33:12 +0400

    init du projet

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # kub-digital
+Npm : https-errors express path cookie-parser morgan nodemon body-parser express-fileupload
+
+A faire:
+
+En cours
+
+Terminer
+
+Prob
---
+++ routes/indexRoute.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+let indexController = require("../controllers/indexController");
+
+router.get('/', indexController.index);
+
+module.exports = router;
